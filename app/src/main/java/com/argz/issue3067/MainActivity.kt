ce32721336f530dd55acd3e2207e40c53b5d0bc8
argzdev <alvinrustan@google.com>
2022-05-06 21:18:02 +0800

    Initial commit

--- app/src/main/java/com/argz/issue3067/MainActivity.kt
+++ app/src/main/java/com/argz/issue3067/MainActivity.kt
@@ -3,11 +3,15 @@ package com.argz.issue3067
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "remoteConfig"
@@ -15,9 +19,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Thread {
+        GlobalScope.launch {
             remoteConfig()
-        }.start()
+        }
     }
 
     /***
@@ -25,23 +29,19 @@ class MainActivity : AppCompatActivity() {
      */
     fun remoteConfig() {
         Log.d(TAG, "remoteConfig: ------starting------")
-        val timeout: Long = 5;
-
+        val remoteConfigFetchInterval: Long = 5
         val firebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
         firebaseRemoteConfig.setConfigSettingsAsync(
             FirebaseRemoteConfigSettings.Builder()
-                .setMinimumFetchIntervalInSeconds(5)
-                .setFetchTimeoutInSeconds(timeout)
+                .setFetchTimeoutInSeconds(10)
                 .build()
-        )
+        ).await()
+        Log.d(TAG, "remoteConfig: added")
+        firebaseRemoteConfig.fetch(remoteConfigFetchInterval).await()
+        Log.d(TAG, "remoteConfig: fetching")
+        firebaseRemoteConfig.activate().await()
+        Log.d(TAG, "remoteConfig: activating")
+    }
 
-        firebaseRemoteConfig.fetchAndActivate()
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    Log.d(TAG, "Fetch and activate succeeded " + firebaseRemoteConfig.getDouble("random_value"))
-                } else {
-                    Log.d(TAG, "Fetch fails after 60 seconds instead of 5 seconds")
-                }
-            }
-        }
+    private fun <T> Task<T>.await() = Tasks.await(this)
 }
